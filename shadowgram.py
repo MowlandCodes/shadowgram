47c21f245b08f87f156647b1ecb45308d6b2deac
MowlandCodes <mowlandgaming@gmail.com>
2025-07-12 23:46:44 +0700

    edit: bot initialization

--- shadowgram.py
+++ shadowgram.py
@@ -1,20 +1,18 @@
 from telegram import Update
-from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
-                          MessageHandler, filters)
+from telegram.ext import (ApplicationBuilder, CommandHandler, MessageHandler,
+                          filters)
 
-from config.config import *
-from handlers import *
-
-# Features
-
+from config import BOT_TOKEN, LOG_GREEN, LOG_YELLOW
+from handlers import message_handlers, start_command
 
 if __name__ == "__main__":
     # Initializing the Bot
-    print("Initializing Shadowgram Bot...")
+    print(f"{LOG_YELLOW('INIT INFO')} Initializing Shadowgram Bot...")
     bot = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # Bot Commands
-    bot.add_handler(CommandHandler("start", start))
+    # Handlers
+    bot.add_handler(CommandHandler("start", start_command))
+    bot.add_handler(MessageHandler(filters.TEXT, message_handlers))
 
-    print("Running Shadowgram Bot...")
-    bot.run_polling()
+    print(f"{LOG_GREEN('SUCCESS')} Running Shadowgram Bot...")
+    bot.run_polling(poll_interval=3, allowed_updates=Update.ALL_TYPES)
