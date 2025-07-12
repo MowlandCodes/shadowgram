c7f9b982db5ddebbfe634148f28e20c818de16b6
MowlandCodes <mowlandgaming@gmail.com>
2025-07-12 23:47:33 +0700

    add: added a couple of command handlers

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,7 +2,11 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         f"Hi, Shadowgram is online. You can use /help to get started."
     )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Help command is not available yet.")
