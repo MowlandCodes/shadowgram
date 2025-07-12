a3c6f6c13ae740601e48aa38fc5cb4d7c7f96a8c
MowlandCodes <mowlandgaming@gmail.com>
2025-07-12 23:45:32 +0700

    edit: add colorization for log

--- config/config.py
+++ config/config.py
@@ -1,10 +1,18 @@
 import os
 
+from colorama import Back, Fore, Style, init
 from dotenv import load_dotenv
 
 # Loading the Environment Variables
 load_dotenv()
 
+init()
+
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 CHAT_ID = str(os.environ.get("CHAT_ID"))
 OP_ID = str(os.environ.get("OP_ID"))
+
+
+LOG_RED = lambda x: f"{Back.RED}{Fore.BLACK}{Style.BRIGHT} {x} {Style.RESET_ALL}"
+LOG_YELLOW = lambda x: f"{Back.YELLOW}{Fore.BLACK}{Style.BRIGHT} {x} {Style.RESET_ALL}"
+LOG_GREEN = lambda x: f"{Back.GREEN}{Fore.BLACK}{Style.BRIGHT} {x} {Style.RESET_ALL}"
