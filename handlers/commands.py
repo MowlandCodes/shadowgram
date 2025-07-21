5f14a5d6345ec5591e219f72d7db7dcbcb89d52d
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 16:52:43 +0700

    add: added some upcoming command

--- handlers/commands.py
+++ handlers/commands.py
@@ -12,3 +12,15 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_markdown_v2(HELP_TEXT)
+
+
+async def list_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+async def interact_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+async def shell_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
