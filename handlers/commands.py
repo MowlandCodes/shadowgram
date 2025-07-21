382c196695902c56d1110ab848513f9ecdb8b993
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 13:04:37 +0700

    edit: reply message using Markdown instead of Plaintext

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,12 +1,14 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from config import *
+
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        f"Hi, Shadowgram is online. You can use /help to get started."
+    await update.message.reply_markdown_v2(
+        r"""Hi, Shadowgram is *online*\. You can use /help to get started\."""
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help command is not available yet.")
+    await update.message.reply_markdown_v2(HELP_TEXT)
