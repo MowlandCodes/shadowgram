6c11aecb5eac9e7cba25e24ec0207de9ce1f423d
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 13:05:02 +0700

    edit: some fixation, and using logger instead of print

--- shadowgram.py
+++ shadowgram.py
@@ -1,18 +1,21 @@
+import logging
+
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, CommandHandler, MessageHandler,
                           filters)
 
-from config import BOT_TOKEN, LOG_GREEN, LOG_YELLOW
-from handlers import message_handlers, start_command
+from config import BOT_TOKEN, LOG_GREEN, LOG_YELLOW, log_gram
+from handlers import help_command, message_handlers, start_command
 
 if __name__ == "__main__":
     # Initializing the Bot
-    print(f"{LOG_YELLOW('INIT INFO')} Initializing Shadowgram Bot...")
+    log_gram.debug("Initializing Shadowgram Bot...")
     bot = ApplicationBuilder().token(BOT_TOKEN).build()
 
     # Handlers
     bot.add_handler(CommandHandler("start", start_command))
+    bot.add_handler(CommandHandler("help", help_command))
     bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handlers))
 
-    print(f"{LOG_GREEN('SUCCESS')} Running Shadowgram Bot...")
+    log_gram.info("Bot is Online!")
     bot.run_polling(poll_interval=2, allowed_updates=Update.ALL_TYPES)
