26d109ecabd01f062764dddd69fa5da1bbf5c67c
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 13:19:03 +0700

    edit: separate between File Logging & Console Logging

--- config/logger.py
+++ config/logger.py
@@ -4,7 +4,25 @@ import time
 from colorama import Fore, Style
 
 
-class LogFormatter(logging.Formatter):
+class FileFormatter(logging.Formatter):
+    format = f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] - %(name)s - [%(levelname)s]: %(message)s"
+    datefmt = "%d-%b-%Y %H:%M:%S"
+
+    FORMATS = {
+        logging.DEBUG: f"{format}",
+        logging.INFO: f"{format}",
+        logging.WARNING: f"{format}",
+        logging.ERROR: f"{format}",
+        logging.CRITICAL: f"{format}",
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, self.datefmt)
+        return formatter.format(record)
+
+
+class ConsoleFormatter(logging.Formatter):
     debug = Fore.WHITE
     informative = Fore.BLUE
     warning = Fore.YELLOW
@@ -31,6 +49,10 @@ class LogFormatter(logging.Formatter):
 log_gram = logging.getLogger("Shadowgram")
 log_gram.setLevel(logging.DEBUG)
 
-handler = logging.StreamHandler()
-handler.setFormatter(LogFormatter())
-log_gram.addHandler(handler)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(ConsoleFormatter())
+file_handler = logging.FileHandler("shadowgram.log")
+file_handler.setFormatter(FileFormatter())
+
+log_gram.addHandler(file_handler)  # For File logging
+log_gram.addHandler(stream_handler)  # For Console logging
