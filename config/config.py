6c15217ffe12d3441683fc6294e4e7e0fa97a1bc
MowlandCodes <mowlandgaming@gmail.com>
2025-07-21 11:27:55 +0700

    edit: i don't think it's necessary...

--- config/config.py
+++ config/config.py
@@ -11,7 +11,6 @@ init()
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 BOT_USERNAME = str(os.environ.get("BOT_USERNAME"))
 CHAT_ID = str(os.environ.get("CHAT_ID"))
-OP_ID = str(os.environ.get("OP_ID"))
 
 # Victim Bot (To interact with victim)
 ALPHA_BOT_TOKEN = str(os.environ.get("ALPHA_BOT_TOKEN"))
