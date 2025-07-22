0a4745f4d50194ea9fc5cdd3818bafb9a72cd84f
MowlandCodes <mowlandgaming@gmail.com>
2025-07-22 09:21:31 +0700

    edit: separate message between channel & group

--- handlers/message.py
+++ handlers/message.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from config import BOT_USERNAME, log_gram
+from config import ALPHA_BOT_USERNAME, BOT_USERNAME, log_gram
 
 
 def handle_text_message(text) -> str:
@@ -14,22 +14,42 @@ def handle_text_message(text) -> str:
             return "I don't understand what you're trying to say."
 
 
-async def message_handlers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message_type: str = update.message.chat.type
-    text: str = update.message.text
-    sender_id = update.message.from_user.id
-    message_title = update.message.chat.title
+async def message_handlers(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    message_type = update.effective_message.chat.type
 
-    if message_type == "group" or message_type == "supergroup":
-        if BOT_USERNAME in text:
-            response = text.replace(BOT_USERNAME, "").strip()
-            response = handle_text_message(response)
-        else:
-            response = handle_text_message(text)
-    else:
-        response = handle_text_message(text)
+    match message_type:
+        case "group" | "supergroup":
+            message_text: str = update.effective_message.text
+            sender_id = update.effective_message.from_user.id
 
-    # Bot response
-    log_gram.info(f"User: ({sender_id}) in Chat({message_title}): '{text}'")
-    log_gram.info(f"Bot: '{response}'")
-    await update.message.reply_text(response)
+            if BOT_USERNAME in message_text:
+                response = message_text.replace(BOT_USERNAME, "").strip()
+                response = handle_text_message(response)
+            else:
+                response = handle_text_message(message_text)
+
+            log_gram.info(f"User ({sender_id}) in Group: '{message_text}'")
+            log_gram.info(f"Bot: '{response}'")
+            await update.effective_message.reply_text(response)
+
+        case "channel":
+            message_text: str = update.effective_message.text
+
+            if BOT_USERNAME in message_text or ALPHA_BOT_USERNAME in message_text:
+                response = (
+                    message_text.replace(BOT_USERNAME, "")
+                    .replace(ALPHA_BOT_USERNAME, "")
+                    .strip()
+                )
+                response = handle_text_message(response)
+            else:
+                response = handle_text_message(message_text)
+
+            log_gram.info(
+                f"Channel ({update.effective_message.chat.title}) posted: '{message_text}'"
+            )
+            log_gram.info(f"Bot: '{response}'")
+            await update.effective_message.reply_text(response)
+
+        case _:
+            pass  # Don't handle message outside of the group or channel
