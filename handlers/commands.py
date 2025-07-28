62d6f449fbdd267609c4ff4a46665f30feff37b3
MowlandCodes <mowlandgaming@gmail.com>
2025-07-28 18:31:24 +0700

    edit: added error handler for the bot

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,7 +1,45 @@
+import json
+import traceback
+
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 
 from config import *
+
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log error and send it to the Developer"""
+
+    # Send warning to user
+    await context.bot.send_message(
+        chat_id=CHAT_ID,
+        text="""⚠️ *An error has Occurred\\!* ⚠️
+        
+Report this problem to *@xhadow21 in Telegram*
+
+> For more information about the error, *check the logs*\\.
+> 
+> Log file: *shadowgram\\.log*
+>
+> _*Do not reply to this message*_""",
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
+    log_gram.error("An error has occurred!", exc_info=context.error)
+
+    # Get all the errors and convert it into a sendable format
+    tb_error = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_error = "".join(tb_error)
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    update_str = str(json.dumps(update_str, indent=4, ensure_ascii=False))
+    ctx_user = str(context.user_data)
+    ctx_chat = str(context.chat_data)
+
+    log_gram.error(
+        f"Update: {update_str}\nContext (User): {ctx_user}\nContext (Chat): {ctx_chat}\nTraceback: {tb_error}",
+    )
 
 
 async def start_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
