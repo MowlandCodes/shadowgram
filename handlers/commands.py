a0e0214213acf3caa26f265c4ff26e212fa0fd95
MowlandCodes <mowlandgaming@gmail.com>
2025-07-28 13:43:58 +0700

    edit: change the /start and /help message to show the banner

--- handlers/commands.py
+++ handlers/commands.py
@@ -6,12 +6,13 @@ from config import *
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_markdown_v2(
-        r"""Hi, Shadowgram is *online*\. You can use /help to get started\."""
+        f"""{BANNER_TEXT}\n *SHADOWGRAM v0\\.1* \n\nHi, *Shadowgram is online*\\.✅\nYou can use /help to get started\\."""
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_markdown_v2(HELP_TEXT)
+    await update.message.reply_markdown_v2(f"{BANNER_TEXT}{HELP_TEXT}")
 
 
 async def list_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
