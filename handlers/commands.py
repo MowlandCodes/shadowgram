ddd0c19e06feef576563ae5c42869ccd109501dc
MowlandCodes <mowlandgaming@gmail.com>
2025-07-28 13:44:19 +0700

    edit: change the context to _ (cuz it's unused)

--- handlers/commands.py
+++ handlers/commands.py
@@ -4,24 +4,23 @@ from telegram.ext import ContextTypes
 from config import *
 
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_markdown_v2(
         f"""{BANNER_TEXT}\n *SHADOWGRAM v0\\.1* \n\nHi, *Shadowgram is online*\\.✅\nYou can use /help to get started\\."""
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_markdown_v2(HELP_TEXT)
+async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_markdown_v2(f"{BANNER_TEXT}{HELP_TEXT}")
 
 
-async def list_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def list_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     pass
 
 
-async def interact_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def interact_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     pass
 
 
-async def shell_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def shell_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     pass
